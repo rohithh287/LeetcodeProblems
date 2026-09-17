aa7f747882e629a656d65e29ef4e19575154fa8d
Rohith <rohithjaishankar877@gmail.com>
2026-09-17 10:36:21 +0530

    Add files via upload

---
+++ leetcode3.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        k=0
+        for num in nums:
+            if num != val:
+                nums[k]=num
+                k +=1
+        return k    
